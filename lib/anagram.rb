e759e7d789cf652d91cf3c11405dbf58e664f00f
Jeni Dang <jenipdang@yahoo.com>
2022-02-07 16:11:02 -0800

    commit tree 2609dadae9a63aedd38a176e49308b46f10706ba

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    # def match(array)
+    #     array.select { |i| i.split("").sort == @word.split("").sort}
+    # end
+    def match(array)
+        array.select { |i| i.chars.sort == @word.chars.sort}
+    end
+end
+
+
